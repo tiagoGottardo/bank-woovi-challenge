e801c1753057b7eb0dc94138d0c09c39726e9cc0
= <tiago.panizio@hotmail.com>
2024-07-08 18:54:47 -0300

    add account types

---
+++ server/src/types/account.ts
@@ -0,0 +1,25 @@
+export type UUID = string;
+
+export interface CreateAccountInput {
+  name: string;
+  email: string;
+  password: string;
+  account_key: string;
+  date_of_birth: Date;
+  cpf: string;
+}
+
+export interface IAccount {
+  _id: UUID;
+  name: string;
+  email: string;
+  password: string;
+  account_key: string;
+  date_of_birth: Date;
+  cpf: string;
+  balance_in_cents: number;
+}
+
+export interface Context {
+  user: string;
+}
