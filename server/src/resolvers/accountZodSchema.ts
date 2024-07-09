c8fbd3d8a3f3f4f31ab7453e2a17022aa8133aa8
= <tiago.panizio@hotmail.com>
2024-07-08 21:48:38 -0300

    fix date of birth zod validation

--- server/src/resolvers/accountZodSchema.ts
+++ server/src/resolvers/accountZodSchema.ts
@@ -1,25 +1,22 @@
 import { z } from 'zod'
 
-const today = new Date();
-const eighteenYearsAgo = new Date(today.setFullYear(today.getFullYear() - 18));
-
 function validateCpf(cpf: string): boolean {
-  const cpfDigits = cpf.split('').map(Number);
+  const cpfDigits = cpf.split('').map(Number)
 
   if (cpfDigits.length !== 11 || cpfDigits.some(isNaN)) {
-    return false;
+    return false
   }
 
   const calculateCheckDigit = (digits: number[], factor: number): number => {
-    let sum = digits.reduce((acc, digit, index) => acc + digit * (factor - index), 0);
-    let remainder = sum % 11;
-    return remainder < 2 ? 0 : 11 - remainder;
-  };
+    let sum = digits.reduce((acc, digit, index) => acc + digit * (factor - index), 0)
+    let remainder = sum % 11
+    return remainder < 2 ? 0 : 11 - remainder
+  }
 
-  const firstCheckDigit = calculateCheckDigit(cpfDigits.slice(0, 9), 10);
-  const secondCheckDigit = calculateCheckDigit(cpfDigits.slice(0, 10), 11);
+  const firstCheckDigit = calculateCheckDigit(cpfDigits.slice(0, 9), 10)
+  const secondCheckDigit = calculateCheckDigit(cpfDigits.slice(0, 10), 11)
 
-  return cpfDigits[9] === firstCheckDigit && cpfDigits[10] === secondCheckDigit;
+  return cpfDigits[9] === firstCheckDigit && cpfDigits[10] === secondCheckDigit
 }
 
 const AccountSchema = z.object({
@@ -36,10 +33,24 @@ const AccountSchema = z.object({
   account_key: z.string().min(8).max(50).regex(/^[A-Za-z0-9_]+$/, {
     message: "Account key must contain only letters, numbers, and underscores.",
   }),
-  date_of_birth: z.date().max(eighteenYearsAgo, {
-    message: "You must be at least 18 years old.",
-  }),
-  cpf: z.string().length(11).regex(/^\d+$/, {
+  date_of_birth: z.string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid date format. Expected YYYY-MM-DD.")
+    .transform((str) => {
+      const date = new Date(`${str}T00:00:00.000Z`)
+      if (isNaN(date.getTime())) {
+        throw new Error("Invalid date.")
+      }
+      return date
+    })
+    .refine((date) => {
+      const now = new Date()
+      const eighteenYearsAgo = new Date(now.getFullYear() - 18, now.getMonth(), now.getDate())
+
+      return date <= eighteenYearsAgo
+    }, "Date must be at least 18 years ago."),
+  cpf: z.string().length(11, {
+    message: "CPF must contain exactly 11 digits"
+  }).regex(/^\d+$/, {
     message: "CPF must contain only digits.",
   }).refine(validateCpf, {
     message: "Invalid CPF number.",
