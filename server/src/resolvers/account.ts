e531ac117ce7b15741bdaefe94bcfbea1154b78f
= <tiago.panizio@hotmail.com>
2024-07-08 21:49:19 -0300

    update register and login resolvers

--- server/src/resolvers/account.ts
+++ server/src/resolvers/account.ts
@@ -1,44 +1,84 @@
-import User from '../models/User'
+import Account from '../models/Account'
+import { CreateAccountInput, LoginAccountInput } from '../types/account'
+
+import { z } from 'zod'
+import accountZodSchema from './accountZodSchema'
+
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 
-interface userRequest {
-  username: string,
-  password: string
-}
-
-const JWT_SECRET = process.env.JWT_SECRET || ''
+const JWT_SECRET = process.env.JWT_SECRET || 'other-secret'
 
 export default {
   Query: {
-    me: async (_: any, __: any, { user }: { user: any }) => {
-      return User.findOne({ username: user.username })
+    me: async () => {
+      return "me"
+      // return Account.findOne({ username: user.username })
     },
   },
   Mutation: {
-    login: async (_: any, { username, password }: userRequest) => {
-      const user = await User.findOne({ username })
-      if (!user) {
-        throw new Error('User not found');
+    login: async (_: undefined, args: LoginAccountInput) => {
+      const { email, password } = args
+      const account = await Account.findOne({ email })
+      if (!account) {
+        throw new Error('Account not found');
       }
 
-      const valid = await bcrypt.compare(password, user.password)
-
+      const valid = await bcrypt.compare(password, account.password)
       if (!valid) {
         throw new Error('Invalid password');
       }
 
-      return jwt.sign({ username }, JWT_SECRET, { expiresIn: '1h' })
+      return jwt.sign({ email }, JWT_SECRET, { expiresIn: '1h' })
     },
-    register: async (_: any, { username, password }: userRequest) => {
-      if (await User.findOne({ username })) {
-        throw new Error('User already exists')
+    register: async (_: undefined, args: CreateAccountInput) => {
+      try {
+        accountZodSchema.parse(args)
+        console.table(args)
+
+        const { email, cpf, account_key, name, password, date_of_birth } = args
+
+        const existingAccount = await Account.findOne({
+          $or: [
+            { email },
+            { cpf },
+            { account_key }
+          ]
+        });
+
+        if (existingAccount) {
+          if (existingAccount.email === email) {
+            throw new Error('An account already uses that email.');
+          } else if (existingAccount.cpf === cpf) {
+            throw new Error('An account already has that CPF.');
+          } else {
+            throw new Error('An account already uses that key.');
+          }
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const account = new Account({
+          name,
+          email,
+          cpf,
+          account_key,
+          date_of_birth,
+          password: hashedPassword,
+        })
+        await account.save()
+
+        return jwt.sign({ email }, JWT_SECRET, { expiresIn: '1h' })
+      } catch (e) {
+        if (e instanceof z.ZodError) {
+          console.error('Validation error:', e.errors[0].message);
+          return e.errors[0].message
+        }
+
+        console.error(e);
+        throw new Error('Failed to create account, ' + (e as Error).message);
       }
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = new User({ username, password: hashedPassword })
-      await user.save();
-      return jwt.sign({ username }, JWT_SECRET, { expiresIn: '1h' })
+
     },
   },
 }
-
