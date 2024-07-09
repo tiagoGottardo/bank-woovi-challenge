7a4a1cba22c0635a70bb5e5b2ce50ceab67f69d4
= <tiago.panizio@hotmail.com>
2024-07-09 11:52:13 -0300

    pass token to graphql

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,6 @@ import { makeExecutableSchema } from '@graphql-tools/schema'
 import { createHandler } from 'graphql-http/lib/use/koa'
 
 import { renderPlaygroundPage } from 'graphql-playground-html';
-import jwt from 'jsonwebtoken'
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -23,40 +22,25 @@ const schema = makeExecutableSchema({
 const app = new Koa()
 const router = new Router()
 
-const authenticate = async (ctx: Koa.Context, next: () => Promise<any>) => {
-  const token = ctx.headers.authorization?.replace('Bearer ', '') || ''
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || '')
-    ctx.user = decoded;
-  } catch (e) {
-    ctx.user = null;
-  }
-
-  await next();
-};
-
-app.use(authenticate);
-
 router.all('/playground', async (ctx: Koa.Context) => {
   const endpoint = '/graphql';
-  // const context = { user: ctx?.user };
 
   const playground = renderPlaygroundPage({
-    endpoint,
+    endpoint
   });
 
   ctx.body = playground;
 });
 
-
 router.all('/graphql', createHandler({
   schema,
   context: (ctx: any) => ({
-    user: ctx?.user,
+    token: ctx?.headers?.authorization
   })
 }))
 
-app.use(router.routes()).use(router.allowedMethods())
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 export default app
