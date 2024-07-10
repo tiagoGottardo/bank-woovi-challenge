9caf5d849159202486e05c284ac8c9b3ee341c90
= <tiago.panizio@hotmail.com>
2024-07-10 03:18:36 -0300

    modularize environments variables

---
+++ server/src/config.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const config = {
+  PORT: process.env.PORT || 3001,
+  MONGO_URI: process.env.MONGO_URI as string,
+  JWT_SECRET: process.env.JWT_SECRET as string || 'woovi-secret',
+  NODE_ENV: process.env.NODE_ENV,
+}
