fcad1faab4293f8b5a64863758aacc55a912e70a
= <tiago.panizio@hotmail.com>
2024-07-12 01:13:13 -0300

    add mask prop to custom input component

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -1,50 +1,56 @@
-import React from 'react'
-
-import { Input as ShadcnInput } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import React, { useEffect, useRef } from 'react';
+import IMask from 'imask';
+import { Input as ShadcnInput } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export type InputProps = {
-  id: string,
-  type: string,
-  label?: string,
-  value: string,
-  touched: boolean | undefined,
-  error?: string
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
-  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void,
-}
+  id: string;
+  type: string;
+  label?: string;
+  value: string;
+  touched?: boolean;
+  mask?: string;
+  error?: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
+};
 
 const Input: React.FC<InputProps> = ({
-  id,
-  type,
   error,
   label,
-  value,
   touched,
-  onChange,
-  onBlur
+  mask,
+  ...rest
 }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current && mask) {
+      const maskInstance = IMask(inputRef.current, { mask });
+      return () => maskInstance.destroy();
+    }
+  }, [mask]);
+
   return (
     <div className="text-start m-8 my-0">
-      <Label className={`${touched && error && "text-red-600"}`}>{label}</Label>
+      {label && (
+        <Label className={`${touched && error ? "text-red-600" : ""}`}>
+          {label}
+        </Label>
+      )}
       <ShadcnInput
+        ref={inputRef}
         className={`
           h-12 rounded-sm focus-visible:border-2 
-          ${touched && error ?
-            "focus-visible:border-red-600 ring-0 border-red-600"
-            : "focus-visible:border-woovi focus-visible:ring-0  hover:border-black hover:border-1"}
+          ${touched && error
+            ? "focus-visible:border-red-600 ring-0 border-red-600"
+            : "focus-visible:border-woovi focus-visible:ring-0 hover:border-black hover:border-1"}
           `}
-
-        id={id}
-        type={type}
-        onBlur={onBlur}
-        onChange={onChange}
-        value={value}
-        required
+        {...rest}
       />
       {touched && error && <div className="text-red-600 text-sm">{error}</div>}
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Input
+export default Input;
