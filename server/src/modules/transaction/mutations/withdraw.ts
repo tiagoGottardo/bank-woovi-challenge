a64ccce1830342f72b078f8a83228ba0a4793de1
= <tiago.panizio@hotmail.com>
2024-07-17 12:59:00 -0300

    fix withdraw error message

--- server/src/modules/transaction/mutations/withdraw.ts
+++ server/src/modules/transaction/mutations/withdraw.ts
@@ -15,7 +15,7 @@ export const withdrawMutation = mutationWithClientMutationId({
   mutateAndGetPayload: async ({ idempotencyKey, amount_in_cents }, { account }: GraphQLContext) => {
     if (!account) { throw new Error("Token is not valid!") }
     if (amount_in_cents <= 0) { throw new Error("Amount must be greater than 0.") }
-    if (account.balance_in_cents - amount_in_cents < 0) { throw new Error("Your withdrawal amount is greater than your balance.") }
+    if (account.balance_in_cents - amount_in_cents < 0) { throw new Error("Your withdraw amount is greater than your balance.") }
 
     const existingTransaction = await TransactionModel.findOne({ _id: idempotencyKey })
     if (existingTransaction) { throw new Error("That transaction already was done.") }
